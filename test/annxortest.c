3c9ce44f17ce2da95ad517c09b632563597a7679
echoline <401497+echoline@users.noreply.github.com>
2018-10-17 20:42:19 -0700

    test both training algorithms

--- test/annxortest.c
+++ test/annxortest.c
@@ -11,17 +11,30 @@ main()
 	double outputs[4] = { 0.0, 1.0, 1.0, 0.0 };
 	double error = 1000;
 
-	//test->rate = 4.0;
-
+	print("testing anntrain()\n");
 	while (error >= 0.001) {
 		error = 0;
 		for (i = 0; i < 4; i++)
-			error += anntrain_adam(test, inputs[i], &outputs[i]);	
-
+			error += anntrain(test, inputs[i], &outputs[i]);	
 		counter++;
 		if (counter % 10000 == 1)
 			print("error: %f\n", error);
 	}
+	print("error: %f, done after %d epochs\n", error, counter);
 
+	for (i = test->n-2; i >= 0; i--)
+		weightsinitrand(test->weights[i]);
+	error = 1000;
+	counter = 0;
+
+	print("testing anntrain_adam()\n");
+	while (error >= 0.001) {
+		error = 0;
+		for (i = 0; i < 4; i++)
+			error += anntrain_adam(test, inputs[i], &outputs[i]);	
+		counter++;
+		if (counter % 10000 == 1)
+			print("error: %f\n", error);
+	}
 	print("error: %f, done after %d epochs\n", error, counter);
 }
